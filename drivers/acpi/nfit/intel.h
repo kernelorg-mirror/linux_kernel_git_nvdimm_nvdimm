118f8adfae02e675fcab38e113bd7de034dff07d
Dave Jiang <dave.jiang@intel.com>
2018-10-08 16:55:35 -0700

    acpi/nfit: Add support for Intel DSM 1.7 commands
    Add command definition for security commands defined in Intel DSM
    specification v1.7. This includes "get security state", "set passphrase",
    "unlock unit", "freeze lock", "secure erase", "ovewrite", and
    "overwrite query". Since we are adding a lot of Intel definitions, moving
    the relevant bits to its own header. Also, we don't want those commands
    to be issued from user space through ioctls. Reason being some of the
    security commands require kernel involvement to flush all CPU caches that
    can't be done in userspace and the result can cause system crash. So
    blocking security commands in the ioctl path.
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/acpi/nfit/intel.h
+++ drivers/acpi/nfit/intel.h
@@ -35,4 +35,65 @@ struct nd_intel_smart {
 	};
 } __packed;
 
+/*
+ * NVDIMM security support requires global cache invalidation (wbinvd),
+ * this is hard coded for x86 for now.
+ */
+#ifdef CONFIG_X86
+
+#define ND_INTEL_STATUS_SIZE		4
+#define ND_INTEL_PASSPHRASE_SIZE	32
+
+#define ND_INTEL_STATUS_RETRY		5
+#define ND_INTEL_STATUS_NOT_READY	9
+#define ND_INTEL_STATUS_INVALID_STATE	10
+#define ND_INTEL_STATUS_INVALID_PASS	11
+
+#define ND_INTEL_SEC_STATE_ENABLED	0x02
+#define ND_INTEL_SEC_STATE_LOCKED	0x04
+#define ND_INTEL_SEC_STATE_FROZEN	0x08
+#define ND_INTEL_SEC_STATE_PLIMIT	0x10
+#define ND_INTEL_SEC_STATE_UNSUPPORTED	0x20
+
+struct nd_intel_get_security_state {
+	u32 status;
+	u32 reserved;
+	u8 state;
+	u8 reserved1[3];
+} __packed;
+
+struct nd_intel_set_passphrase {
+	u8 old_pass[ND_INTEL_PASSPHRASE_SIZE];
+	u8 new_pass[ND_INTEL_PASSPHRASE_SIZE];
+	u32 status;
+} __packed;
+
+struct nd_intel_unlock_unit {
+	u8 passphrase[ND_INTEL_PASSPHRASE_SIZE];
+	u32 status;
+} __packed;
+
+struct nd_intel_disable_passphrase {
+	u8 passphrase[ND_INTEL_PASSPHRASE_SIZE];
+	u32 status;
+} __packed;
+
+struct nd_intel_freeze_lock {
+	u32 status;
+} __packed;
+
+struct nd_intel_secure_erase {
+	u8 passphrase[ND_INTEL_PASSPHRASE_SIZE];
+	u32 status;
+} __packed;
+
+struct nd_intel_overwrite {
+	u8 passphrase[ND_INTEL_PASSPHRASE_SIZE];
+	u32 status;
+} __packed;
+
+struct nd_intel_query_overwrite {
+	u32 status;
+} __packed;
+#endif /* CONFIG_X86 */
 #endif
